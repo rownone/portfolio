9e42b618d09103302a9c421f14abb084b9273ec5
rona <row.none@gmail.com>
2022-11-27 16:44:20 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
+import '../styles/globals.css'
 
-import '../styles.css'
-
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-//import styles from '../styles/Home.module.css';
-//import '../styles.css'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-      <div >
+    <div className={styles.container}>
       <Head>
         <title>Ronan Lee</title>
         <link rel="icon" href="/favicon.ico" />
