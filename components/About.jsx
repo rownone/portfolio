9b03156802ef7183cfceff2708b62d2179c71dbf
ron <row.none@gmail.com>
2023-08-20 06:04:40 -0700

    fixed spacing

--- components/About.jsx
+++ components/About.jsx
@@ -15,13 +15,13 @@ function About() {
                 I am a Web Developer with over 18 years of experience in developing, debugging, and deploying diverse software and web applications.
 				<br/>
 				In web development, I use Yii, CodeIgniter, Next.js, React.js, Web3, Python, jQuery, WordPress, HTML, CSS, and MySQL for databases. I have also utilized GIT and SVN to facilitate team-oriented development.
-				<br/>
+				<br/><br/>
 				I have developed APIs using serverless compute services with AWS Lambda functions. I have deployed smart contracts on Ethereum and Binance networks for various projects and managed Top-Level Domains on (HNS) Handshake. I also created chatbot AI with custom knowledge base.
-				<br/>
+				<br/><br/>
 				I can work independently, but I also thrive in a team environment where I can collaborate and contribute.
-				<br/>
+				<br/><br/>
 				I excel at resolving application problems; my colleagues can always rely on me when they are unable to fix bugs and glitches in their work. 
-				<br/>
+				<br/><br/>
 				I am passionate about coding and committed to delivering high-quality solutions.
             </p>
             <div className="social-icons">
